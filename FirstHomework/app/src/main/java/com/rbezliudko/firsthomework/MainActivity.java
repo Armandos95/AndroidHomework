4ca961881f4cf54d73d17bf4c0438b6c79c44055
Roman Bezliudko <bezl1995@ukr.net>
2015-10-10 17:18:20 +0300

    first homework v1.1

--- FirstHomework/app/src/main/java/com/rbezliudko/firsthomework/MainActivity.java
+++ FirstHomework/app/src/main/java/com/rbezliudko/firsthomework/MainActivity.java
@@ -4,18 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText parameterA;
     private EditText parameterB;
     private EditText parameterC;
-    private Button button;
-    private TextView result;
+    private Button buttonResult;
+    private TextView textResult;
+    private CheckBox checkBoxComplex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +25,18 @@ public class MainActivity extends AppCompatActivity {
         parameterA = (EditText) findViewById(R.id.edit_text_1);
         parameterB = (EditText) findViewById(R.id.edit_text_2);
         parameterC = (EditText) findViewById(R.id.edit_text_3);
-        button = (Button) findViewById(R.id.button);
-        result = (TextView) findViewById(R.id.text_result);
+        buttonResult = (Button) findViewById(R.id.button_result);
+        textResult = (TextView) findViewById(R.id.text_result);
+        checkBoxComplex = (CheckBox) findViewById(R.id.checkbox_complex);
+        if (checkBoxComplex.isChecked()) checkBoxComplex.setChecked(false);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonResult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    result.setText(makeResult());
-                } catch (NumberFormatException ex)
-                {
-                    result.setText(R.string.error_message);
+                    textResult.setText(makeResult());
+                } catch (NumberFormatException ex) {
+                    textResult.setText(R.string.error_message);
                 }
             }
         });
@@ -43,20 +44,32 @@ public class MainActivity extends AppCompatActivity {
 
     private String makeResult()
     {
-        int a, b, c;
-        a = Integer.parseInt(parameterA.getText().toString());
-        b = Integer.parseInt(parameterB.getText().toString());
-        c = Integer.parseInt(parameterC.getText().toString());
-        int discriminant = b * b - 4 * a * c;
+        double a, b, c;
+        a = Double.parseDouble(parameterA.getText().toString());
+        b = Double.parseDouble(parameterB.getText().toString());
+        c = Double.parseDouble(parameterC.getText().toString());
+        double discriminant = b * b - 4 * a * c;
         String str = "Discriminant: " + discriminant + "\n";
         if (discriminant < 0)
         {
-            str += "There are no real roots.";
-            return str;
+            if (checkBoxComplex.isChecked())
+            {
+                discriminant *= -1;
+                double realPart = -1 * b / (2 * a);
+                double complexPart = Math.sqrt(discriminant) / (2 * a);
+                str += "x1 = " + realPart + " + " + complexPart + "i \n";
+                str += "x2 = " + realPart + " - " + complexPart + "i";
+                return str;
+            }
+            else
+            {
+                str += "There are no real roots.";
+                return str;
+            }
         }
         else if (discriminant == 0)
         {
-            int x = -1 * b / (2 * a);
+            double x = -1 * b / (2 * a);
             str += "x = " + x;
             return str;
         }
