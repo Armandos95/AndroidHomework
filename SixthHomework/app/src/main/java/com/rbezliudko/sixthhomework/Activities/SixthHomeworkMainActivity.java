3134c11187cba6fb0c74f0d02c06a4185bb6cf9d
Roman Bezliudko <bezl1995@ukr.net>
2015-11-23 23:04:52 +0200

    sixth homeworks works now!

--- SixthHomework/app/src/main/java/com/rbezliudko/sixthhomework/Activities/SixthHomeworkMainActivity.java
+++ SixthHomework/app/src/main/java/com/rbezliudko/sixthhomework/Activities/SixthHomeworkMainActivity.java
@@ -1,33 +1,27 @@
 package com.rbezliudko.sixthhomework.Activities;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.rbezliudko.sixthhomework.Fragments.SixthHomeworkAFragment;
 import com.rbezliudko.sixthhomework.Fragments.SixthHomeworkBFragment;
 import com.rbezliudko.sixthhomework.Interfaces.ButtonMethodsInterfaceSix;
 import com.rbezliudko.sixthhomework.R;
 
 public class SixthHomeworkMainActivity extends AppCompatActivity implements ButtonMethodsInterfaceSix {
 
-    private FragmentManager manager = getSupportFragmentManager();
-
-    private SixthHomeworkAFragment fragmentA = (SixthHomeworkAFragment) getFragmentManager().findFragmentById(R.id.fragment_6_a);
-    private SixthHomeworkBFragment fragmentB = (SixthHomeworkBFragment) getFragmentManager().findFragmentById(R.id.fragment_6_b);
+    private SixthHomeworkBFragment fragmentB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sixth_homework_main);
+        fragmentB  = (SixthHomeworkBFragment) getFragmentManager().findFragmentById(R.id.fragment_6_b);
     }
 
     public void showZergling() {
         if (fragmentB == null) {
-            Intent intent = new Intent(SixthHomeworkMainActivity.this, SixthHomeworkBFragment.class);
+            Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "zergling");
             startActivity(intent);
         } else {
@@ -37,7 +31,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
 
     public void showMarauder() {
         if (fragmentB == null) {
-            Intent intent = new Intent(SixthHomeworkMainActivity.this, SixthHomeworkBFragment.class);
+            Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "marauder");
             startActivity(intent);
         } else {
@@ -47,7 +41,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
 
     public void showQueen() {
         if (fragmentB == null) {
-            Intent intent = new Intent(SixthHomeworkMainActivity.this, SixthHomeworkBFragment.class);
+            Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "queen");
             startActivity(intent);
         } else {
@@ -57,7 +51,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
 
     public void showInfestor() {
         if (fragmentB == null) {
-            Intent intent = new Intent(SixthHomeworkMainActivity.this, SixthHomeworkBFragment.class);
+            Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "infestor");
             startActivity(intent);
         } else {
@@ -67,7 +61,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
 
     public void showZealot() {
         if (fragmentB == null) {
-            Intent intent = new Intent(SixthHomeworkMainActivity.this, SixthHomeworkBFragment.class);
+            Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "zealot");
             startActivity(intent);
         } else {
@@ -77,7 +71,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
 
     public void showStalker() {
         if (fragmentB == null) {
-            Intent intent = new Intent(SixthHomeworkMainActivity.this, SixthHomeworkBFragment.class);
+            Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "stalker");
             startActivity(intent);
         } else {
