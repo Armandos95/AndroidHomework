7d5527821bb66f2fff3bde8ec452a1d298371c24
Roman Bezliudko <bezl1995@ukr.net>
2015-11-23 23:30:16 +0200

    fixed some bugs

--- SixthHomework/app/src/main/java/com/rbezliudko/sixthhomework/Activities/SixthHomeworkMainActivity.java
+++ SixthHomework/app/src/main/java/com/rbezliudko/sixthhomework/Activities/SixthHomeworkMainActivity.java
@@ -3,6 +3,7 @@ package com.rbezliudko.sixthhomework.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.rbezliudko.sixthhomework.Fragments.SixthHomeworkBFragment;
 import com.rbezliudko.sixthhomework.Interfaces.ButtonMethodsInterfaceSix;
@@ -11,16 +12,20 @@ import com.rbezliudko.sixthhomework.R;
 public class SixthHomeworkMainActivity extends AppCompatActivity implements ButtonMethodsInterfaceSix {
 
     private SixthHomeworkBFragment fragmentB;
+    private String layoutType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sixth_homework_main);
+        TextView tempText = (TextView) findViewById(R.id.text_fragment_a);
+        tempText.setText(R.string.layout_type);
+        layoutType = tempText.getText().toString();
         fragmentB  = (SixthHomeworkBFragment) getFragmentManager().findFragmentById(R.id.fragment_6_b);
     }
 
     public void showZergling() {
-        if (fragmentB == null) {
+        if (layoutType.equals("one_fragment")) {
             Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "zergling");
             startActivity(intent);
@@ -30,7 +35,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
     }
 
     public void showMarauder() {
-        if (fragmentB == null) {
+        if (layoutType.equals("one_fragment")) {
             Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "marauder");
             startActivity(intent);
@@ -40,7 +45,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
     }
 
     public void showQueen() {
-        if (fragmentB == null) {
+        if (layoutType.equals("one_fragment")) {
             Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "queen");
             startActivity(intent);
@@ -50,7 +55,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
     }
 
     public void showInfestor() {
-        if (fragmentB == null) {
+        if (layoutType.equals("one_fragment")) {
             Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "infestor");
             startActivity(intent);
@@ -60,7 +65,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
     }
 
     public void showZealot() {
-        if (fragmentB == null) {
+        if (layoutType.equals("one_fragment")) {
             Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "zealot");
             startActivity(intent);
@@ -70,7 +75,7 @@ public class SixthHomeworkMainActivity extends AppCompatActivity implements Butt
     }
 
     public void showStalker() {
-        if (fragmentB == null) {
+        if (layoutType.equals("one_fragment")) {
             Intent intent = new Intent(this, SixthHomeworkAdditionalActivity.class);
             intent.putExtra("chosenUnit", "stalker");
             startActivity(intent);
